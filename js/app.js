9ef842bd30708f7ec271f2f99c500d8cf5ee305b
htbradley13 <htbradley13@gmail.com>
2016-08-16 22:11:26 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -12,6 +12,24 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+  	/* newGame function */
+  	function newGame (){
+  		//Resets the guess entry input
+  		$(".text").reset();
+  		//Resets guess counter
+  		$("#count").reset();
+  		//Removes list of guessed numbers
+  		$("#guessList").remove("li")
+  	}
+
+  	/* Call newGame function on button click */
+  	$(".new").click(function newGame());
+
+  	
+
 });
+
+
 
 
