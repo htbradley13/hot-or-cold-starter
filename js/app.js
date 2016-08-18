0eaec33fd04dce7958873b38ffbf30bc66f61ee2
htbradley13 <htbradley13@gmail.com>
2016-08-17 23:19:15 -0400

    saving3

--- js/app.js
+++ js/app.js
@@ -11,30 +11,28 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-    //Call/Invoking newGame function
-    newGame();
 
     /* Call/invoke newGame function on button click */
     $(".new").click(newGame());
 
 });
 
+    /* Declaring randomNumber function */
+    function randomNumber (min, max){
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     /* Call/Invoking randomNumber function with our range parameters for between 1-100, and setting it as a variable */
     var randomNumberSelected = randomNumber(1,100);
-
-    /* Validate user guess is between 1 and 100 */
-    function guessValidation () {
-      if ($("#userGuess").val() > 100 || $("#userGuess").val() < 1) {
-      alert("Please enter a number between 1 - 100");
-      }
-    }
 
   	/* Declaring newGame function */
   	function newGame (){
   		//Resets the guess entry input
-  		$(".text").reset();
+  		$("form").reset();
   		//Resets guess counter
-  		$("#count").reset();
+  		$("#count").val("0");
   		//Removes list of guessed numbers
   		$("#guessList").remove("li")
       //Clears any feedback types from old games
@@ -45,13 +43,6 @@ $(document).ready(function(){
       $("#feedback").show();
   	}
 
-  	/* Declaring randomNumber function */
-  	function randomNumber (min, max){
-  		min = Math.ceil(min);
-  		max = Math.floor(max);
-  		return Math.floor(Math.random() * (max - min + 1)) + min;
-  	}
-
     /* Setting user count total variable, convert to number from string as well */
     var userGuessCountTotal = +($("#count").val());
 
@@ -60,23 +51,26 @@ $(document).ready(function(){
           var newTally = x + 1;
           $("#count").html("<span>" + newTally + "</span");
     }
-    
 
-
+    /* Declare function to ensure user guess is between 1-100, if not alert them */
+    function guessValidation () {
+      if (+($("#userGuess").val()) > 100 || +($("#userGuess").val()) < 1) {
+      alert("Please enter a number between 1 - 100");
+      }
+    }
 
   	/* Feedback function on submit*/
   	$("#guessButton").submit(function(event){
-      //Since we aren't submitting to a server
-  		event.preventDefault();
-      /* Set userNumEntry variable to the user's guessed number, also convert value from string to integer with + symbol, could use parseInt() as an alternative*/
-      var userNumEntry = +($("#userGuess").val());
-      /* Validate user's guess is between 1-100, if not alert them */
-      if (userNumEntry < 1 || userNumEntry > 100) {
-        alert("Please enter a number between 1-100");
-      }
-      else {
+        //Since we aren't submitting to a server
+  		  event.preventDefault();
+        //Invoke guess validation
+        guessValidation();
+        /* Set userNumEntry variable to the user's guessed number, also convert value from string to integer with + symbol, could use parseInt() as an alternative*/
+        var userNumEntry = +($("#userGuess").val());
         /* Invoke user guess tally function and hide current feedback showing*/
         guessCountTally(userGuessCountTotal);
+        /* Add each guess to the li for user guesses */
+        $("#guessList").append("<li>" + userNumEntry + "</li>")
         $("#feedback").hide();
         /* Then show feedback based on the below */
         if (((userNumEntry - randomNumberSelected).abs()) = 0) {
@@ -88,9 +82,10 @@ $(document).ready(function(){
         else if (((userNumEntry - randomNumberSelected).abs()) >= 11) {
           $("#feedbackCold").show();
         }
-      }
 
   	});
+
+
 
 
 
